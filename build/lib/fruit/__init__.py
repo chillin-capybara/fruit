260037188c40831ee02934633d399fe9ab463248
Marcell Pigniczki <marceip97@gmail.com>
2020-04-08 11:24:34 +0200

    Clean Repo

--- build/lib/fruit/__init__.py
+++
@@ -1,4 +0,0 @@
-from fruit.modules.finder import *
-from fruit.modules.job import *
-from fruit.modules.step import *
-from fruit.modules.target import *
--- release.sh
+++
@@ -1,5 +0,0 @@
-rm dist/*
-m2r readme.md
-python3 setup.py sdist
-python3 setup.py bdist_wheel sdist
-python3 -m twine upload dist/*
